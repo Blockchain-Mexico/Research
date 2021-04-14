82320b5efecbe210fffc8489abf75d3768d0a8c1
olmeca <sdfasd>
2021-04-13 22:58:36 -0500

    Polynomial for resolving the hibryd in a Descentralized way

---
+++ polynomial_systems/multisamples_combs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>;
+#include <cmath>;
+#include <random>;
+// an a priori unknown quantity for a desired state
+// position of the leader seems important 
+void multisubset() {
+    int numbers = number[:]
+    int subsets = {}
+    int output = {}
+
+    for roundcount auto && _range = 9999999 {
+        pair_count = {};
+        for (index = subsets, subsets.items); {
+            for (x : subsets) {
+                for (y : subsets) {
+                    if (y > x) {
+                        pair_count[(x, y)] = pair_count.get((x, y), 0) + 1;
+                    }
+                }
+            }
+        }
+        pair_count = sort([el : pair_count.keys()]), auto glambda = key[]
+    }
+}
--- polynomial_systems/poly_utils.cpp
+++ polynomial_systems/poly_utils.cpp
@@ -3,5 +3,19 @@
 
 class PrimeFiled() {
 
+    void add(int x, int y) {
+        return x + y % modulus
+    }
 
+    void sub(int x, int y) {
+        return x - y % modulus
+    }
+
+    void mul(int x, int y) {
+        return x * y % modulus
+    }
+
+    void exp(int x, int y) {
+        return 
+    }
 }
