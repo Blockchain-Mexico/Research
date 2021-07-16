714737ed40590dd82908261dedde3f4ad5265970
Your Name <you@example.com>
2021-07-16 01:29:52 -0600

    Spec

---
+++ consensus/maya.py
@@ -0,0 +1,14 @@
+
+
+
+// good ideas in the multideminsional
+// https://arxiv.org/pdf/2105.13487.pdf
+def block():
+    
+
+    self.shoter_of_samples
+    self.n_samples
+    # the other stuff could be a similiar spec as eth 2.0 !
+    # overwheling! 
+
+
