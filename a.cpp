73e0e7218a86e322d545af2b4debe0aa904e063a
MacBook Air <macbookair@MacBook-Air-de-MacBook.local>
2021-10-16 17:11:01 -0500

    Lypanov analysis

---
+++ a.cpp
@@ -0,0 +1,31 @@
+
+// lypanov method 
+//  How to do ? 
+
+void lypanov_method() {
+	// is non trivial?
+	//  # of intergres doing a singlurarity
+	//  How to escape of this >------
+	//  To do this? ----<
+}
+
+class block() {
+	// just add the eth 2 string stuff
+	//  add the shooter and ?
+	//  The hard part is to prove non trivail the lypanov method works
+}
+
+// the c++ is silly ? 
+// We nees a special lypanov lexeme for this ? 
+
+int main() {
+	int n;
+	shooter_sample() /// here is trivial?
+}
+
+// https://scholar.google.com/scholar?hl=es&as_sdt=0,5&q=lyapunov+method
+/*
+ *
+ *
+ *  [] --- [lypanov_method() lexeme on chain?]
+ *
