261f578d65ed24b9b0d48a8972399e5a263da517
MacBook Air <macbookair@MacBook-Air-de-MacBook.local>
2021-10-27 17:38:11 -0500

    mad_max-trip debug

---
+++ DEX/Mad-max-trip.cpp
@@ -0,0 +1,15 @@
+
+
+
+// Thread about the root movement that is a imposibility in DEX approach 
+
+
+// but the 0/0 attack is the ranoware of crypto 
+// intruction just to send to mad max a token *any kind of language the burn the key and burn the boy.
+
+
+// the king of rats? well the is tranparents rats I mean a tweak rat so you know ehre the token in your funtioncal haskell bullshit
+
+
+
+//homework: just look for the Peter W. lib attach the string a play
