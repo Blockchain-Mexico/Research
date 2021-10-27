2e8b9d8fd8a5eab02d3cd9160935973f3ca7c8c5
MacBook Air <macbookair@MacBook-Air-de-MacBook.local>
2021-10-27 17:31:19 -0500

    Anywat fft ransowarejust do a hybrid

---
+++ ransoware_fetich/a.cpp
@@ -0,0 +1,19 @@
+
+// https://github.com/leonv024/RAASNet/blob/master/src/morse_encoder.py
+// this was the leet code version that was patch 
+// but recreate a B of Atcoder in dp movement or possibly fft: move of the root
+// fact: the cryptographic is protect the strings but no the root so you need the hybrid solution 
+//   [root] --> [the easy] just a one 
+//   so now  adn root 
+//   [A][G][C] thas start to move in recursion that you figuerin out because is ftt crazy
+
+
+// homework: just look the B of atcoder dp example and attach the string to non-trivial implemntation or this variant of ransoware to erase a replicate data
+
+void anyway() {
+
+}
+
+int main() {
+	int
+}
