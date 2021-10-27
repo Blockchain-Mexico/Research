c64f9e2e16707eda66be73b907becb1eae02b71b
MacBook Air <macbookair@MacBook-Air-de-MacBook.local>
2021-10-27 17:50:01 -0500

    0-0 burn token attack that dont care how to protect

---
+++ Our_fund_migration/a.cpp
@@ -0,0 +1,14 @@
+
+
+
+
+
+## the retrance infinite imposility
+
+
+
+# trick the mad max just copy paste this in blockchain stuff for the imutability of data 
+
+#the hard part? how to migrate to bio-stuff or just to 0 all
+
+
