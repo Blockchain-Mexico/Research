eb2863eb3b29c81c21ab3f3ec28125b451d403d0
MacBook Air <macbookair@MacBook-Air-de-MacBook.local>
2021-10-27 17:44:02 -0500

    Blue de cagada

---
+++ blu_cielo_cagada/a.cpp
@@ -0,0 +1,14 @@
+### Waiting for the blue - you know because there is copy stuff 
+
+
+// open source that is just a clone of the sam thread
+// the same attack
+// bug of followers: farm memes in facebook and put in twitter 
+// beacuse the rationale desing is just for pay fees for be popular
+
+
+Calculate how much cost be the Elon bug?
+150 M follower like 10 billion 
+65.k memes cringe latin a not blue arrow aceptance because was plagarism 
+tik tok bucket overwrite a machine learning to just overwrite the khaby lame 
+
